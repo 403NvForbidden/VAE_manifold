2863882970f28648c97ab8b9e7c0aa7fbc845a79
403NvForbidden <z5155332@student.unsw.edu.au>
2021-07-29 08:52:22 +1000

    rerun dsprite

--- Code/refactor_run_2stageVAE.py
+++ Code/refactor_run_2stageVAE.py
@@ -4,6 +4,7 @@
 import datetime
 import os
 import pandas as pd
+from scipy.stats import norm
 
 import pytorch_lightning as pl
 import torch
@@ -13,6 +14,7 @@ import plotly.offline
 
 from models.networks_refactoring import twoStageInfoMaxVAE, twoStageVaDE, twoStageBetaVAE
 from models.train_net import VAEXperiment, pretrain_2stageVaDE_model_SSIM, pretrain_2stageVAEmodel_SSIM
+from quantitative_metrics.classifier_metric import dsprite_classifier_performance
 from quantitative_metrics.performance_metrics_single import compute_perf_metrics
 from quantitative_metrics.unsupervised_metric import save_representation_plot
 from util.config import args, dataset_lookUp, device
@@ -25,13 +27,13 @@ from torchsummary import summary
 # specific argument for this model
 from util.helpers import metadata_latent_space, plot_from_csv, get_raw_data, double_reconstruciton
 
-args.add_argument('--model', default='twoStageInfoMaxVaDE_2')
+args.add_argument('--model', default='twoStageInfoMaxVaDE')
 args.add_argument('--zdim1', dest="hidden_dim_aux", type=float, default=100)
 args.add_argument('--alpha', type=float, default=1)
 args.add_argument('--beta', type=float, default=1)
 args.add_argument('--gamma', type=float, default=1)
 args.add_argument('--pretrained', dest='weight_path', type=str,
-                  default='/mnt/Linux_Storage/outputs/1_experiment/pretrian_twoStageVADE_6/logs/last.ckpt')
+                  default='/mnt/Linux_Storage/outputs/2_dsprite/pretrainDsprite_infoMaxVAE/logs/last.ckpt')
 args = args.parse_args()
 # TODO: overwrite the parameters
 
@@ -72,7 +74,7 @@ Experiment = VAEXperiment(model, {
 # pretrain_2stageVAEmodel_SSIM(model, train_loader, pre_epoch=30, save_path=save_model_path, device=device)
 pretrain_2stageVaDE_model_SSIM(model, train_loader, pre_epoch=30, save_path=save_model_path, device=device)
 
-# Experiment.load_weights(args.weight_path)
+Experiment.load_weights(args.weight_path)
 
 # define the logger to log training output, the default is using tensorBoard
 logger = pl_loggers.TensorBoardLogger(f'{save_model_path}/logs/', name=args.model)
@@ -93,13 +95,13 @@ if args.train and args.weight_path == '':
     # call tune to find the batch size
     # trainer.tune(Experiment, train_loader)
     trainer.fit(Experiment, train_loader, valid_loader)
-
 ##########################################################
 # %% Evaluate
 ##########################################################
 ## step 1 ##
 ## transform the imgs in the dataset to latent dimensions
 # prepare the inference dataset
+
 _, infer_dataloader = get_inference_dataset(dataset_path, batchsize=args.batch_size,
                                                      input_size=args.input_size)
 
@@ -127,40 +129,44 @@ except:
 
     double_reconstruciton(infer_dataloader, model, save_model_path, device, num_img=12, logger=logger)
 
+test_acc, mean_acc, std_acc  = dsprite_classifier_performance(metadata_csv, low_dim_names=['z0', 'z1', 'z2'])
+print(test_acc)
+accuracy_df = pd.DataFrame({'test_accuracies_': test_acc, 'mean_acc': mean_acc, 'std_acc_m1': std_acc})
+accuracy_df.to_csv(os.path.join(save_model_path, 'classifier_acc_score.csv'), index=False)
 ###############################
 # %% Run performance matrics ###
 ###############################
-params_preferences = {
-    'feature_size': args.input_size * args.input_size * args.input_channel,
-    'path_to_raw_data': dataset_path,
-    # 'path_to_raw_data': '../DataSets/Selected_Hovarth',
-    'dataset_tag': 1,  # 1:BBBC 2:Horvath 3:Chaffer
-    'low_dim_names': ['z0', 'z1', 'z2'],
-    'global_saving_path': save_model_path + '/',  # Different for each model, this one is update during optimization
-
-    ### Unsupervised metrics
-    'save_unsupervised_metric': False,
-    'only_local_Q': False,
-    'kt': 300,
-    'ks': 500,
-
-    ### Mutual Information
-    'save_mine_metric': False,
-    'batch_size': 256,
-    'bound_type': 'interpolated',
-    'alpha_logit': -4.6,  # result in alpha = 0.01
-    'epochs': 10,
-
-    ### Classifier accuracy
-    'save_classifier_metric': True,
-    'num_iteration': 3,
-
-    ### BackBone Metric
-    'save_backbone_metric': False,
-
-    ### Disentanglement Metric
-    'save_disentanglement_metric': False,
-    'features': dataset_lookUp[args.dataset]['feat'],
-}
-compute_perf_metrics(metadata_csv, params_preferences, logger)
-logger.close()
+# params_preferences = {
+#     'feature_size': args.input_size * args.input_size * args.input_channel,
+#     'path_to_raw_data': dataset_path,
+#     # 'path_to_raw_data': '../DataSets/Selected_Hovarth',
+#     'dataset_tag': 1,  # 1:BBBC 2:Horvath 3:Chaffer
+#     'low_dim_names': ['z0', 'z1', 'z2'],
+#     'global_saving_path': save_model_path + '/',  # Different for each model, this one is update during optimization
+#
+#     ### Unsupervised metrics
+#     'save_unsupervised_metric': False,
+#     'only_local_Q': False,
+#     'kt': 300,
+#     'ks': 500,
+#
+#     ### Mutual Information
+#     'save_mine_metric': False,
+#     'batch_size': 256,
+#     'bound_type': 'interpolated',
+#     'alpha_logit': -4.6,  # result in alpha = 0.01
+#     'epochs': 10,
+#
+#     ### Classifier accuracy
+#     'save_classifier_metric': True,
+#     'num_iteration': 3,
+#
+#     ### BackBone Metric
+#     'save_backbone_metric': False,
+#
+#     ### Disentanglement Metric
+#     'save_disentanglement_metric': False,
+#     'features': dataset_lookUp[args.dataset]['feat'],
+# }
+# compute_perf_metrics(metadata_csv, params_preferences, logger)
+# logger.close()
